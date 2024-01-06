9a2ac30fdeed53cf8a6262d0c37d82574834490c
Leonardwkwong <weikit.wong.2019@scis.smu.edu.sg>
2024-01-06 21:40:56 +0800

    Role.java

--- src/main/java/.com/models/Role.java
+++ src/main/java/.com/models/Role.java
@@ -3,4 +3,18 @@ package com.springjwt.models;
 import jakarta.persistence.*;
 
 @Entity
-@Ta
+@Table(name = "roles")
+
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role(){
+        
+    }
+}
